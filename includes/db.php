47ef74a89c36a99802b446d59cd965b360d36e5e
Samuel <samuelanyaele@gmail.com>
2019-02-28 18:13:28 +0100

    Database Connection

---
+++ includes/db.php
@@ -0,0 +1,5 @@
+<?php
+	$link = mysqli_connect ("us-cdbr-iron-east-03.cleardb.net", "b4c14df3598e7f", "6ecb13ff", "heroku_81ef78cbb358d0a?") or die ("Connection Error: " . mysqli_connect_error());
+	//$link = mysqli_connect ("localhost", "person41_smstk", "tummymouse", "person41_smstk") or die ("Connection Error: " . mysqli_connect_error());
+	// mysql://b4c14df3598e7f:6ecb13ff@us-cdbr-iron-east-03.cleardb.net/heroku_81ef78cbb358d0a?reconnect=true
+?>
